1c1ed9a6097ab6ecbdb4ef1b17b010c0719c126f
Frank Wolf <fwolf@posteo.de>
2023-05-26 16:56:18 +0200

    :sparkles: add deck, :bug: tidy up card code

--- nbdev_cards/card.py
+++ nbdev_cards/card.py
@@ -3,16 +3,14 @@
 # %% auto 0
 __all__ = ['suits', 'ranks', 'Card']
 
-# %% ../nbs/00_card.ipynb 2
-from nbdev.showdoc import *
-from fastcore.test import *
+# %% ../nbs/00_card.ipynb 3
 from fastcore.utils import *
 
-# %% ../nbs/00_card.ipynb 3
+# %% ../nbs/00_card.ipynb 4
 suits = ["♣️","♠️","❤️","♦️"]
 ranks = [None, None] + [str(x) for x in range(2,11)] + ["J", "Q", "K", "A"]
 
-# %% ../nbs/00_card.ipynb 12
+# %% ../nbs/00_card.ipynb 13
 class Card: 
     'A playing card'
     def __init__(self, 
@@ -23,28 +21,28 @@ class Card:
     __repr__ = __str__
     
 
-# %% ../nbs/00_card.ipynb 19
+# %% ../nbs/00_card.ipynb 20
 @patch
 def __eq__(self: Card, other:Card): return (self.suit, self.rank)==(other.suit, other.rank)
 
-# %% ../nbs/00_card.ipynb 21
+# %% ../nbs/00_card.ipynb 22
 Card(suit=1, rank=1) == Card(suit=1, rank=1)
 
-# %% ../nbs/00_card.ipynb 22
+# %% ../nbs/00_card.ipynb 23
 Card(suit=1, rank=1) == Card(suit=2, rank=1)
 
-# %% ../nbs/00_card.ipynb 24
+# %% ../nbs/00_card.ipynb 25
 @patch
 def __lt__(self: Card, other:Card): return self.rank < other.rank
 
-# %% ../nbs/00_card.ipynb 25
+# %% ../nbs/00_card.ipynb 26
 # Example
 Card(suit=1, rank=1) < Card(suit=2, rank=2)
 
-# %% ../nbs/00_card.ipynb 27
+# %% ../nbs/00_card.ipynb 28
 Card(suit=1, rank=1) > Card(suit=2, rank=1)
 
-# %% ../nbs/00_card.ipynb 29
+# %% ../nbs/00_card.ipynb 30
 @patch
 def __gt__(self: Card, other:Card): return self.rank > other.rank
 
